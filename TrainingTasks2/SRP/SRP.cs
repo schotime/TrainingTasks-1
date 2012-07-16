dfc2013911bb04c9bfb5b3240c510559fc42fd97
Adam Schroder <adamschroder@gmail.com>
2012-07-17 09:16:38 +1000

    SRP - Extract methods

--- TrainingTasks2/SRP/SRP.cs
+++ TrainingTasks2/SRP/SRP.cs
@@ -7,15 +7,31 @@ namespace TrainingTasks2.SRP
     {
         public void Register(string email, string password)
         {
-            if (!email.Contains("@"))
-                throw new Exception("Email is not an email!");
+            ValidateEmail(email);
 
             var user = new User(email, password);
+            SaveUser(user);
+
+            SendEmail(email);
+        }
+
+        private static void SaveUser(User user)
+        {
             var db = new FakeDatabase();
             db.Save(user);
+        }
 
+        private static void ValidateEmail(string email)
+        {
+            if (!email.Contains("@"))
+                throw new Exception("Email is not an email!");
+        }
+
+        private static void SendEmail(string email)
+        {
             var smtpClient = new FakeSmtpClient();
-            smtpClient.Send(new MailMessage("mysite@nowhere.com", email) { Subject = "Hello fool!" });
+            smtpClient.Send(new MailMessage("mysite@nowhere.com", email) {Subject = "Hello fool!"});
         }
     }
+
 }
